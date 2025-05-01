0b88ec74c09d6c04cfa51cfbe6a1ed8cd7d458d9
superchanuwu <36646270+superchanuwu@users.noreply.github.com>
2025-05-01 21:50:57 +0700

    Update ambatukam_patched.js

--- ambatukam_patched.js
+++ ambatukam_patched.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "jembut.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "cloud"; // Ganti dengan nama workers kalian
+const rootDomain = "kameha.workers.dev"; // Ganti dengan domain utama kalian
+const serviceName = "cipok"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
